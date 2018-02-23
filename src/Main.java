552886973d94071a1e758e67ff7c17605d9c0ddb
Leonard Gehin <leonard.gehin@gmail.com>
2018-02-23 18:52:41 +0100

    Ajout attenteJoueur()

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
 
 
 public class Main{
@@ -11,15 +13,16 @@ public class Main{
 		 * Mise en place du serveur TCP et des connnections avec les clients
 		 * Port: 
 		 */
-		int[] socketTravail = new int[nombreJoueurMax];
+		Socket[] socketTravail = new Socket[nombreJoueurMax];
 		
 		//Creation du socket principal
 		int port = 6000;
 		boolean portValide = false;
+		ServerSocket SocketPrincipal = null;
 		while(!portValide && port <= Math.pow(2, 16)-1) { //Si un port est deja utilise, il faut en prendre un autre
             portValide = true;
 			try {
-	            ServerSocket SocketPrincipal = new ServerSocket(port, nombreJoueurMax);
+	            SocketPrincipal = new ServerSocket(port, nombreJoueurMax);
 			} catch (IOException e) {
 				System.out.println("Port "+port+" est deja utilise.");
 				port++;
@@ -34,8 +37,69 @@ public class Main{
 		else {
 			System.out.println("Port utilile : " + port + ".");
 			
+			//Time out sur le socket principal pour le rendre non bloquant
+			try {
+				SocketPrincipal.setSoTimeout(100);
+			} catch (SocketException e) {
+				e.printStackTrace();
+			}
+			socketTravail = attenteJoueur(SocketPrincipal, nombreJoueurMax);
 		}
+	}				
+	
+	
+	//Renvoie les sockets de travail pour communiquer avec les clients
+	public static Socket[] attenteJoueur(ServerSocket SocketPrincipal, int nombreJoueurMax) {
+		Socket[] socketTravail = new Socket[nombreJoueurMax];
+		boolean[] joueurPret = new boolean[nombreJoueurMax];
+		boolean[] timeOutSet = new boolean[nombreJoueurMax];
+		int nombreJoueurConnecte = 0;
+		boolean attenteClient = true;
+		
+		for (int i = 0; i < socketTravail.length; i++) socketTravail[i] = null;
+		
+		while(attenteClient && nombreJoueurConnecte < nombreJoueurMax) {
+			try {
+				//Accept la connection
+				socketTravail[nombreJoueurConnecte] = SocketPrincipal.accept();
+				
+				//Nouveau socket accepte
+				if(socketTravail[nombreJoueurConnecte] != null) {
+					//Mise en place du timeOut pour ce nouveau socket
+					if(!timeOutSet[nombreJoueurConnecte]) {
+						try {
+							socketTravail[nombreJoueurConnecte].setSoTimeout(8);
+						} catch (SocketException e) {
+							e.printStackTrace();
+						}
+					}
+					
+					nombreJoueurConnecte++;
+				}
+				
+				//Ecoute si les clients sont pret
+				
+				//Si un joueur n'est pas pret, on attend
+				attenteClient = false;
+				for (int i = 0; i < joueurPret.length; i++) 
+					if(!joueurPret[i])
+						attenteClient = true;
+			} catch (IOException e) {
+				e.printStackTrace(); //Affiche l'erreur
+			}
+		}
+		
+		return socketTravail;
 	}
+	
+	/*
+	//Ecoute les messages sur tout les sockets et renvoie un message recu
+	//Les sockets ecoute sont les sockets non nuls
+	public static String ecouteClient(Socket[] socketTravail) {
+		String message = "";
+		return message;
+	}
+	*/
 }
 
 
